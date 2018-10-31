c236e62134ac47fa2cd45950559b10df311baa4c
David Spörri <davepermen@hotmail.com>
2018-10-31 13:26:01 +0100

    Updater added (part one)

---
+++ src/UpdateFromGithub/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace UpdateFromGithub
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            IApplicationLifetime applicationLifetime = app.ApplicationServices.GetRequiredService<IApplicationLifetime>();
+
+            app.Run(async (context) =>
+            {
+                if (context.Request.Method == "POST")
+                {
+                    File.WriteAllText("request.txt", await new StreamReader(context.Request.Body).ReadToEndAsync());
+                    await context.Response.WriteAsync(await new StreamReader(context.Request.Body).ReadToEndAsync());
+                    //applicationLifetime.StopApplication();
+                }
+                await context.Response.WriteAsync("");
+            });
+        }
+    }
+}
