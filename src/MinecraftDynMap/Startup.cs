4a71f1cf52b43c5b9605e724f24fcd9414a5cb4e
David Spörri <davepermen@hotmail.com>
2018-11-01 21:30:00 +0100

    fixed proxy

--- src/MinecraftDynMap/Startup.cs
+++ src/MinecraftDynMap/Startup.cs
@@ -1,40 +1,15 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using System.IO;
 
 namespace MinecraftDynMap
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) => app.RunProxy(new ProxyOptions()
         {
-            app.UseProxyRoute("mc.davepermen.net", "localhost", 8123);
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("minecraft dynmap proxy");
-            });
-        }
-    }
-
-    public static class ProxyMapHelper
-    {
-        public static IApplicationBuilder UseProxyRoute(this IApplicationBuilder builder, string domain, string host, int port = 80, string scheme = "http")
-        {
-            builder.UseWhen(
-                _ => _.Request.Host.Host == domain,
-                app => app.Use(async (context, next) =>
-                {
-                    context.Request.Host = new HostString(host);
-                    await next.Invoke();
-                }).RunProxy(new ProxyOptions()
-                {
-                    Host = host,
-                    Port = port.ToString(),
-                    Scheme = scheme
-                })
-            );
-            return builder;
-        }
+            Host = "localhost",
+            Port = 8123.ToString(),
+            Scheme = "http"
+        });
     }
 }
