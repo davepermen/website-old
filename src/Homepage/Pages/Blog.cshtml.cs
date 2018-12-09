488ed0e5a0dd384603b1bcaa67d3d9d6e73e546d
David Spörri <davepermen@hotmail.com>
2018-12-09 11:18:12 +0100

    improved the OnGet.. (was kinda obvious but, oh well.. learning...)

--- src/Homepage/Pages/Blog.cshtml.cs
+++ src/Homepage/Pages/Blog.cshtml.cs
@@ -1,7 +1,6 @@
 ﻿using Markdig;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using System;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -16,24 +15,20 @@ namespace Homepage.Pages
         public string BlogContent => blogContent ?? "";
 
 
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet(string slug)
         {
-            if (RouteData.Values["Slug"] is string slug)
+            var path = $@"{Program.DataRoot}\blog\{slug}\index.md";
+            if (path.Contains(@"\..\") == false && System.IO.File.Exists(path))
             {
-                var path = $@"{Program.DataRoot}\blog\{slug}\index.md";
-                if (path.Contains(@"\..\") == false && System.IO.File.Exists(path))
-                {
-                    var markdown = await System.IO.File.ReadAllTextAsync(path);
-                    var blog = Markdown.ToHtml(markdown, pipeline);
+                var markdown = await System.IO.File.ReadAllTextAsync(path);
+                var blog = Markdown.ToHtml(markdown, pipeline);
 
-                    var hero = $"/blog/{slug}/hero.jpg";
+                var hero = $"/blog/{slug}/hero.jpg";
 
-                    blogContent = AddSections(blog.ReplaceFirst("<h1>", $"<h1 style=\"background-image: url('{hero}')\">"));
-                    return Page();
-                }
-                return NotFound($"route '{slug}' is not a valid blogpost");
+                blogContent = AddSections(blog.ReplaceFirst("<h1>", $"<h1 style=\"background-image: url('{hero}')\">"));
+                return Page();
             }
-            throw new Exception("RouteData dependency on 'Slug' in Blog.cshtml  is not working anymore");
+            return NotFound($"route '{slug}' is not a valid blogpost");
         }
 
         // yes. yes it works. no, i won't comment.
