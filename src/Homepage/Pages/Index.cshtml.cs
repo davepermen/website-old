31d35414e2ca0b6401356ab6a535466fc3d0163c
David Spörri <davepermen@hotmail.com>
2018-12-09 13:16:53 +0100

    index page cleaned up, logic now in the viewmodel

--- src/Homepage/Pages/Index.cshtml.cs
+++ src/Homepage/Pages/Index.cshtml.cs
@@ -1,16 +1,22 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Homepage.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<(string Title, string Path)> BlogPosts { get; private set; } = null;
+
         public void OnGet()
         {
+            BlogPosts = Directory.GetDirectories($@"{Program.DataRoot}\blog").Select(p =>
+            {
+                var name = Path.GetFileName(p);
+
+                return (Title: name.Replace('-', ' '), Path: name);
+            });
         }
     }
 }
