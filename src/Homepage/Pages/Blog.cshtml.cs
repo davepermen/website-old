466dd873311f37c925661a55c50ef6d9045d88ad
David Spörri <davepermen@hotmail.com>
2018-12-09 11:00:27 +0100

    refactored blog-display code into the BlogModel, cleaned up the Blog Razor Page

--- src/Homepage/Pages/Blog.cshtml.cs
+++ src/Homepage/Pages/Blog.cshtml.cs
@@ -1,17 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Markdig;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace Homepage.Pages
 {
     public class BlogModel : PageModel
     {
-        public void OnGet()
-        {
+        static readonly MarkdownPipeline pipeline = new MarkdownPipelineBuilder().UseYamlFrontMatter().Build();
+        string blogContent = null;
 
+        public bool HasBlogContent => blogContent != null;
+        public string BlogContent => blogContent ?? "";
+
+
+        public async Task<IActionResult> OnGet()
+        {
+            if (RouteData.Values["Slug"] is string slug)
+            {
+                var path = $@"{Program.DataRoot}\blog\{slug}\index.md";
+                if (path.Contains(@"\..\") == false && System.IO.File.Exists(path))
+                {
+                    var markdown = await System.IO.File.ReadAllTextAsync(path);
+                    var blog = Markdown.ToHtml(markdown, pipeline);
+
+                    var hero = $"/blog/{slug}/hero.jpg";
+
+                    blogContent = AddSections(blog.ReplaceFirst("<h1>", $"<h1 style=\"background-image: url('{hero}')\">"));
+                    return Page();
+                }
+                return NotFound($"route '{slug}' is not a valid blogpost");
+            }
+            throw new Exception("RouteData dependency on 'Slug' in Blog.cshtml  is not working anymore");
         }
+
+        // yes. yes it works. no, i won't comment.
+        static string AddSections(string html) => Regex.Replace(html.Replace("<p>", "<section><p>").Replace("</p>", "</p></section>"), @"</section>\s+<section>", "");
     }
 }
