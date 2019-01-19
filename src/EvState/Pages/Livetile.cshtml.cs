fa6e75f760d27b62158349df2c674f9c440734b3
David Spörri <davepermen@hotmail.com>
2019-01-19 13:31:00 +0100

    ev state livetile + image optimisations

--- src/EvState/Pages/Livetile.cshtml.cs
+++ src/EvState/Pages/Livetile.cshtml.cs
@@ -1,16 +1,20 @@
-﻿using Conesoft;
+﻿using EvState.HttpClients;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace EvState.Pages
 {
     public class LivetileModel : PageModel
     {
-        private int amount;
-
-        public void OnGet([FromServices] IDataSources dataSources)
+        public async Task OnGet([FromServices] EVNotifyHttpClient evNotifyHttpClient)
         {
             Response.ContentType = "text/xml";
+
+            StateOfCharge = await evNotifyHttpClient.GetStateOfCharge();
         }
+
+        public float StateOfCharge { get; set; }
+        public float Charged { get; set; }
     }
 }
