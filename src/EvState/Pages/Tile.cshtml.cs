c9dd8e8fd7a23636aec7004b84fce620c5dbdb58
David Spörri <davepermen@hotmail.com>
2019-01-22 21:19:57 +0100

    tile for android, for testing

---
+++ src/EvState/Pages/Tile.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using EvState.HttpClients;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
+
+namespace EvState.Pages
+{
+    public class TileModel : PageModel
+    {
+        public async Task OnGet([FromServices] EVNotifyHttpClient evNotifyHttpClient)
+        {
+            StateOfCharge = await evNotifyHttpClient.GetStateOfCharge();
+        }
+
+        public float StateOfCharge { get; set; }
+
+        public float Charged { get; set; }
+    }
+}
