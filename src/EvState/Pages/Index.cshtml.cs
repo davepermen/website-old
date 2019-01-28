b399a822ac9656dea4e8d46950d56f689662ed6c
David Spörri <davepermen@hotmail.com>
2019-01-28 22:43:04 +0100

    scrollbars fixed on mobile

--- src/EvState/Pages/Index.cshtml.cs
+++ src/EvState/Pages/Index.cshtml.cs
@@ -1,6 +1,10 @@
-﻿using EvState.HttpClients;
+﻿using Conesoft;
+using EvState.HttpClients;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace EvState.Pages
@@ -10,8 +14,12 @@ namespace EvState.Pages
         public ChargingState[] ChargingState { get; private set; }
         public float StateOfCharge { get; private set; }
 
-        public async Task OnGet([FromServices] ECarUpHttpClient eCarUpHttpClient, [FromServices] EVNotifyHttpClient evNotifyHttpClient)
+        private IDataSources dataSources;
+
+        public async Task OnGet([FromServices] ECarUpHttpClient eCarUpHttpClient, [FromServices] EVNotifyHttpClient evNotifyHttpClient, [FromServices] IDataSources dataSources)
         {
+            this.dataSources = dataSources;
+
             var chargingState = await eCarUpHttpClient.State();
 
             var stateOfCharge = await evNotifyHttpClient.GetStateOfCharge();
@@ -19,6 +27,18 @@ namespace EvState.Pages
             this.ChargingState = chargingState;
 
             this.StateOfCharge = stateOfCharge;
+
+            var _ = StartTimer(Request.GetDisplayUrl());
+        }
+
+        private async Task StartTimer(string uri)
+        {
+            System.IO.File.WriteAllText($"{dataSources.LocalDirectory}/{DateTime.Now.ToShortTimeString().Replace(":", ".")}.txt", "");
+            await Task.Delay(TimeSpan.FromMinutes(1));
+            using (var client = new HttpClient())
+            {
+                await client.GetAsync(uri);
+            }
         }
     }
 }
