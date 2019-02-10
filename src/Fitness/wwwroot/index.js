165d30df3158af206f919f9419cdbc5c19517def
David Spörri <davepermen@hotmail.com>
2019-02-10 14:46:42 +0100

    first step on SPAing the fitness page: links are now without pagerefresh

---
+++ src/Fitness/wwwroot/index.js
@@ -0,0 +1,32 @@
+﻿let fixLinks = () => {
+    document.querySelectorAll('a').forEach(element => {
+        element.addEventListener('click', event => {
+            event.preventDefault()
+            let href = element.getAttribute('href')
+            page(href)
+        })
+    })
+}
+
+let loadPage = async context => {
+    if (context.init !== true) {
+        let path = context.pathname
+        let response = await (await fetch(path)).text()
+        let body = {
+            start: response.indexOf('<body>') + '<body>'.length,
+            end: response.indexOf('</body>')
+        }
+        let content = response.substring(body.start, body.end)
+        document.querySelector('body').innerHTML = content
+
+        fixLinks()
+    }
+}
+
+window.onload = () => {
+    page('/', loadPage)
+    page('/menu', loadPage)
+    page()
+
+    fixLinks()
+}
