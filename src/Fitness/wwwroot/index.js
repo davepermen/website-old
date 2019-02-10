e4fd25889e464299dedd69ebd9102ef0d66fb5e8
David Spörri <davepermen@hotmail.com>
2019-02-10 21:33:00 +0100

    forms are SPA now, too... whole fitness page is SPA

--- src/Fitness/wwwroot/index.js
+++ src/Fitness/wwwroot/index.js
@@ -8,25 +8,48 @@
     })
 }
 
-let loadPage = async context => {
-    if (context.init !== true) {
-        let path = context.pathname
-        let response = await (await fetch(path)).text()
-        let body = {
-            start: response.indexOf('<body>') + '<body>'.length,
-            end: response.indexOf('</body>')
-        }
-        let content = response.substring(body.start, body.end)
-        document.querySelector('body').innerHTML = content
+let fixForms = () => {
+    document.querySelectorAll('form').forEach(element => {
+        element.addEventListener('submit', async event => {
+            event.preventDefault()
+            let formData = new FormData(element)
+            let action = element.getAttribute('action')
+            let method = element.getAttribute('method')
+            if (method === 'post') {
+                await fetch(action, {
+                    method,
+                    body: formData,
+                    redirect: 'manual'
+                })
+                page(location.pathname)
+            } else {
+                let queryParameters = [...formData.entries()].map(e => encodeURIComponent(e[0]) + "=" + encodeURIComponent(e[1])).join('&')
+                page(action + '?' + queryParameters)
+            }
+        })
+    });
+}
 
-        fixLinks()
+let loadPage = async context => {
+    let response = await (await fetch(context.path)).text()
+    let body = {
+        start: response.indexOf('<body>') + '<body>'.length,
+        end: response.indexOf('</body>')
     }
+    let content = response.substring(body.start, body.end)
+    document.querySelector('body').innerHTML = content
+
+    fixLinks()
+    fixForms()
 }
 
 window.onload = () => {
     page('/', loadPage)
     page('/menu', loadPage)
-    page()
+    page({
+        dispatch: false
+    })
 
     fixLinks()
+    fixForms()
 }
