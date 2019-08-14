6101b976ef7c6af74cc7a05058f15ed4c6cefc9a
David Spörri <davepermen@hotmail.com>
2019-08-14 20:27:39 +0200

    added youtube

---
+++ src/YouTube/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Conesoft;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace YouTube
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+            services.AddMvc();
+            services.AddDataSources();
+
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromDays(365);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IDataSources dataSource)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseMvc();
+        }
+    }
+}
