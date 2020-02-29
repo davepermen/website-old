b708f64dc548b838bdecc181bc2224b17aa73e22
David Spörri <davepermen@hotmail.com>
2020-02-29 14:16:56 +0100

    tick tock

---
+++ src/Home/API/ScheduleTickerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Home.API
+{
+    [Route("api/scheduler")]
+    [ApiController]
+    public class ScheduleTickerController : ControllerBase
+    {
+        [HttpPost("tick")]
+        public IActionResult PostTick()
+        {
+            return Ok("tock");
+        }
+    }
+}
